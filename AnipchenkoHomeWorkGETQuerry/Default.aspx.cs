b2c88f676ad776580a4e2224c65b73aafd4bbe52
anipchenko.alexey <pyelya@gmail.com>
2018-09-03 12:05:36 +0300

    First commit

---
+++ AnipchenkoHomeWorkGETQuerry/Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AnipchenkoHomeWorkGETQuerry
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            List<string> parameters = new List<string>();
+
+            if (Request.QueryString.AllKeys.Length == 0)
+            {
+                ParamsLabel.Text = @"NO PARAGETERS IN GET QUERY:(<br/>For add GET param:<br/>Add after uri <h5>?yourParamete=yourValue<h5/>";
+            }
+            else
+            {
+                ParamsLabel.Text = "QUERY PARAMETERS:<br/>";
+                foreach (var parameter in Request.QueryString.AllKeys)
+                {
+                    
+                    ParamsLabel.Text += $"{parameter} = {Request.QueryString[parameter]}<br/>";
+                }
+            }
+            
+        }
+    }
+}
---
+++ AnipchenkoHomeWorkGETQuerry/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AnipchenkoHomeWorkGETQuerry.Global" Language="C#" %>
